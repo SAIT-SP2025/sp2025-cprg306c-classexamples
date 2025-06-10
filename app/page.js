1f19a6e23fd045ba8ef0e31c479253c1cc88f431
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2025-06-10 09:15:20 -0600

    Week 6 Examples Part 1

--- app/page.js
+++ app/page.js
@@ -24,6 +24,8 @@ export default function Home() {
         <li><Link href="./week-4/functions/" className={linkStyles}>Week 4 - Functions</Link> </li>
         <li><Link href="./week-4/counter/" className={linkStyles}>Week 4 - Counter</Link> </li>
         <li><Link href="./week-5/" className={linkStyles}>Week 5 - Managed Form</Link> </li>
+        <li><Link href="./week-6/arrays" className={linkStyles}>Week 6 - Array Manipulation</Link> </li>
+        <li><Link href="./week-6/contact" className={linkStyles}>Week 6 - Handling Lists</Link> </li>
       </ul>
     </main>
   );
